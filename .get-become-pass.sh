c2f5ddaf0622ce441e721178bc5eaf23b27fc2ec
Alex Taylor <ataylor@posteo.us>
2024-01-15 20:41:36 -0500

    first commit

---
+++ .get-become-pass.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+set -eou pipefail
+bw get password a48af410-7691-47ab-beb7-b0c10184948c
---
+++ .get-vault-pass.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+set -eou pipefail
+bw get password 9fbab922-5a53-48b7-a0ff-b0e301151b27
